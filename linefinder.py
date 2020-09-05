e60e33d471244e30073754e48e601fc4384fa088
Justin Walrath <walrathjaw@gmail.com>
2020-09-05 17:12:20 -0400

    Now pull delivered-to, to, and return-path from the emails. Also dedup the results before printing.

--- linefinder.py
+++ linefinder.py
@@ -12,11 +12,22 @@ if len(sys.argv) > 2:
 
 searchterm = os.path.join(path, searchpattern)
 
+addresses = []
+
 for filename in glob.glob(searchterm):
     with open(filename, "r") as f:
         line = f.readline()
         while line:
             line = line.strip()
             if(line.startswith("From: ")):
-                print(line[6:])
+                addresses.append(line[6:])
+            if(line.startswith("Delivered-To: ")):
+                addresses.append(line[14:])
+            if(line.startswith("To: ")):
+                addresses.append(line[4:])
+            if(line.startswith("Return-Path: ")):
+                addresses.append(line[13:])
             line = f.readline()
+
+for address in set(addresses):
+    print(address)
